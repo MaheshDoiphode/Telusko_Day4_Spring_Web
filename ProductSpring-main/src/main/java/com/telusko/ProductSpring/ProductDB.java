b6ba1bcd8fb8528c5f8f2fdfb3c02cd2255c8985
MaheshDoiphode <58901842+MaheshDoiphode@users.noreply.github.com>
2023-05-24 19:41:01 +0530

    Delete ProductSpring-main directory

--- ProductSpring-main/src/main/java/com/telusko/ProductSpring/ProductDB.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.ProductSpring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductDB extends JpaRepository<Product, Integer> {
-
-}
